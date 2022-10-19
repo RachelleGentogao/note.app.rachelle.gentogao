237601127656f224cc3cfb495cac85e7b9dfac31
RachelleGentogao <rachelle@gmail.com>
2022-10-19 16:26:10 +0700

    added delete.js, and added update.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const add = require('./add')
 const read = require('./read')
+const write = require('./write')
+const del = require('./delete')
 const present = require('./present')
+
 // get user input
 const cmd = process.argv
 
@@ -20,5 +23,16 @@ if(cmd[2] == 'add') {
 }
 
 if(cmd[2] == 'read') {
+    // Import present
+    const present = require('./present')
+
     present(read())
+}
+if (cmd[2] === "delete") {
+    const id = cmd[3]
+    const oldnote = read()
+    const del = require ('./delete')
+    //delete an item from note array
+    del(id,oldnote)
+    console.log(read)
 }
